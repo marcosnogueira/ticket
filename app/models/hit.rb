a7475f5c03a68c5bb6cb088b950ddfc5f68f4d4d
Marcos Nogueira <marcosnogueira@live.com>
2011-10-12 21:31:39 -0300

    d

--- app/models/hit.rb
+++ app/models/hit.rb
@@ -4,5 +4,5 @@ class Hit < ActiveRecord::Base
   belongs_to :source_url
   belongs_to :source  
   
-  scope :two_weeks_ago, :conditions => ['created_at > :two_weeks', :two_weeks => (Date.today - 14.days)]
+  scope :two_weeks_ago, :conditions => ['created_at > :two_weeks', { :two_weeks => (Date.today - 14.days) }]
 end
